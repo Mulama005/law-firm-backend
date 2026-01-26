03d7565ea5b92186f79c9141ef1bbdde144e58eb
Bonventure Mulama <bonilinux70@gmail.com>
2026-01-26 20:44:44 +0300

    fix submission form

--- api/views.py
+++ api/views.py
@@ -1,15 +1,17 @@
-from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from .models import Consultation
 import json
-
-def google_verification(request):
-    return HttpResponse(
-        "google-site-verification: google87ffcc77f7e2b586.html",
-        content_type="text/plain; charset=utf-8"
-    )
 
 @csrf_exempt
 def contact(request):
     if request.method == "POST":
         data = json.loads(request.body)
+
+        Consultation.objects.create(
+            name=data.get("name"),
+            email=data.get("email"),
+            message=data.get("message"),
+        )
+
         return JsonResponse({"success": True})
