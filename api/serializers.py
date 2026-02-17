61a9f16dee3bf89128212a0cb8111bdfd09e4029
Bonventure Mulama <bonilinux70@gmail.com>
2026-02-17 19:25:52 +0300

    updated subscriber form

--- api/serializers.py
+++ api/serializers.py
@@ -13,7 +13,4 @@ class SubscriberSerializer(serializers.ModelSerializer):
         fields = ["id", "email", "subscribed_at"]
         read_only_fields = ["id", "subscribed_at"]
 
-    def validate_email(self, value):
-        if Subscriber.objects.filter(email=value).exists():
-            raise serializers.ValidationError("You are already subscribed!")
-        return value
+    
