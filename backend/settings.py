cffd59f35afafb1d0c7fae79092fb308d639b4bc
Bonventure Mulama <bonilinux70@gmail.com>
2026-02-17 14:30:26 +0300

    updated settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -117,22 +117,21 @@ TEMPLATES = [
 ]
 
 # =========================
-# DATABASE (POSTGRESQL)
+# DATABASE 
 # =========================
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600,
-        ssl_require=False
-    )
-}
-
-if os.environ.get("RENDER") != "true":  # Render sets RENDER=true
+if os.environ.get("DATABASE_URL"):
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+        "default": dj_database_url.config(
+            conn_max_age=600,
+            ssl_require=True,
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
         }
     }
 
