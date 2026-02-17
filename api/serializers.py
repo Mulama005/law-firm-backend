aacc51992ed1c543a7ef1b288f8d00f9e94dc935
Bonventure Mulama <bonilinux70@gmail.com>
2026-02-17 19:43:20 +0300

    updated subscriber form

--- api/serializers.py
+++ api/serializers.py
@@ -2,15 +2,15 @@ from rest_framework import serializers
 from .models import Consultation
 from .models import Subscriber
 
+
 class ConsultationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Consultation
         fields = "__all__"
+
 
 class SubscriberSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscriber
         fields = ["id", "email", "subscribed_at"]
         read_only_fields = ["id", "subscribed_at"]
-
-    
