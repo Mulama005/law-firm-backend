fe67b7c8db0a18fc71863ac918792c5a9abdff19
Bonventure Mulama <bonilinux70@gmail.com>
2026-02-17 16:05:23 +0300

    updated admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Consultation
+from .models import Subscriber
 
 admin.site.register(Consultation)
+
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ("email", "subscribed_at")
+    search_fields = ("email",)
